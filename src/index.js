b7fde828613c74e1c47a6a6c8aa9b5cf29fa3e52
DovydasTEDS <dovydasjakcinskas@gmail.com>
2023-05-05 21:18:12 +0100

    embeds dpme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
 
 const tedsbot = new Client({
   intents: [
@@ -35,6 +35,14 @@ client.on("interactionCreate", (interaction) => {
         interaction.reply(`${num1} + ${num2} = 21`);
       } else interaction.reply(`${num1} + ${num2} = ${num1 + num2}`);
     }
+    if (interaction.commandName === "links") {
+        const embed = new EmbedBuilder()
+            .setTitle("Links")
+            .setColor('990000')
+            .addFields({ name: 'TeamTEDS', value: `[Website](https://tedps.tk)\n[Discord](https://discord.gg/rMqJ9gsSrU)\n[Twitter](https://twitter.com/teamteds)\n[Github](https://github.com/TeamTEDS)` }, { name: 'DovydasTEDS', value: `[Twitter](https://twitter.com/dovydasteds)\n[Github](https://github.com/DovydasTEDS)\n[Reddit](https://reddit.com/user/DovydasTEDS)\n[Steam](https://steamcommunity.com/id/dovydasteds)\n[Youtube](https://youtube.com/@dovydasteds)`})
+
+        interaction.reply({ embeds: [embed] });
+      }
   }
 });
 
