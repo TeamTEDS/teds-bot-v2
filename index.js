1b4e5accad9f1cfb4cb860bbff6667c55b998af5
DovydasTEDS <dovydasjakcinskas@gmail.com>
2023-05-06 20:33:16 +0100

    eeeeeeee

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Express app!');
-});
-app.listen(3000, () => {
-  console.log('Web server started');
-});
-require('./src/index.js');
+const keepAlive = require(`./server`);
+require('./src/index');
+keepAlive();
